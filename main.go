6c7e704d63f629d9956d6b5984d0392b6451b0ff
syedmrizwan <syed.m.rizwan@outlook.com>
2020-06-17 22:03:28 +0500

    Executed raw query to populate model

--- main.go
+++ main.go
@@ -24,35 +24,70 @@ func testJoins() {
 		fmt.Println(tenants[0].SubscriptionTypes[1].RefType.TypeName)
 	}
 }
-
-func main() {
+func testRawQuery() {
 	db := database.GetConnection()
 
-	tenant := &model.Tenant{
-		Name:          "Rizwan123",
-		Email:         "ee@gmail.com",
-		RootAccountID: 1,
-	}
+	var podConfigurations []model.PodConfiguration
+	query := `select p.id                  pod_id,
+				   p.name                pod_name,
+				   p.status              status,
+				   d.id                  "configuration__datastore_id",
+				   d.name                "configuration__datastore_name",
+				   c.id                  "configuration__cluster_id",
+				   c.name                "configuration__cluster_name",
+				   v.id                  "configuration__vcenter_id",
+				   v.ip_address          "configuration__ip_address",
+				   v.user_name           "configuration__user_name",
+				   v.password            "configuration__password",
+				   rt.type_name          "configuration__type_name",
+				   rt.vapp_template_name "configuration__template_name"
+			from pods p
+					 inner join datastores d on p.datastore_id = d.id
+					 inner join clusters c on p.cluster_id = c.id
+					 inner join vcenters v on c.vcenter_id = v.id
+					 inner join subscription_types st on p.subscription_type_id = st.id
+					 inner join ref_types rt on st.ref_type_id = rt.id
+			where p.id in (6, 7, 8)`
 
-	err := db.Insert(tenant)
+	_, err := db.Query(&podConfigurations, query)
+
 	if err != nil {
-		panic(err)
+		// Error Handler
 	}
-	fmt.Println("Tenant added")
+	fmt.Println(podConfigurations)
+	fmt.Println(podConfigurations[0].Configuration.IpAddress)
 
-	//inserting pod
-	pod := &model.Pod{
-		Name:      "Pod 1",
-		Status:    "Pending",
-		IpAddress: "10.2.0.11",
-	}
+}
 
-	err = db.Insert(pod)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Pod added")
-
-	testJoins()
+func main() {
+	testRawQuery()
+	//db := database.GetConnection()
+	//
+	//tenant := &model.Tenant{
+	//	Name:          "Rizwan123",
+	//	Email:         "ee@gmail.com",
+	//	RootAccountID: 1,
+	//}
+	//
+	//err := db.Insert(tenant)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println("Tenant added")
+	//
+	////inserting pod
+	//pod := &model.Pod{
+	//	Name:      "Pod 1",
+	//	Status:    "Pending",
+	//	IpAddress: "10.2.0.11",
+	//}
+	//
+	//err = db.Insert(pod)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println("Pod added")
+	//
+	//testJoins()
 
 }
---
+++ model/model.go
@@ -0,0 +1,21 @@
+package model
+
+type ConfigurationInfo struct {
+	VcenterId     int64  `json:"vcenter_id"`
+	IpAddress     string `json:"ip_address"`
+	UserName      string `json:"user_name"`
+	Password      string `json:"password"`
+	DatastoreID   int64  `json:"datastore_id"`
+	DatastoreName string `json:"datastore_name"`
+	ClusterID     int64  `json:"cluster_id"`
+	ClusterName   string `json:"cluster_name"`
+	TemplateName  string `json:"template_name"`
+	TypeName      string `json:"type_name"`
+}
+
+type PodConfiguration struct {
+	PodID         int64              `json:"pod_id"`
+	PodName       string             `json:"pod_name"`
+	Configuration *ConfigurationInfo `json:"configuration"`
+	Status        string             `json:"status"`
+}
